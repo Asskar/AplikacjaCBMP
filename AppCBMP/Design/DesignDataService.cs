a4e7cc1c50b1715c417107a717db3774e9362dcf
Asskar <kilinski.m@gmail.com>
2016-09-06 18:42:29 +0200

    Add project files.

---
+++ AppCBMP/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AppCBMP.Model;
+
+namespace AppCBMP.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
---
+++ AppCBMP/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace AppCBMP.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
